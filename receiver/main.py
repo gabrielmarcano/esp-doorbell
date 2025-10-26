b25a28baa55b0a4f300a62651a4767ad297d60d6
Gabriel Marcano <gabrielmarcano120@gmail.com>
2025-10-25 20:07:37 -0400

    Minor format changes

--- receiver/main.py
+++ receiver/main.py
@@ -9,16 +9,19 @@ WEBHOOK_URL = "http://homeassistant:8123/api/webhook/doorbell"
 WIFI_SSID = "SSID"
 WIFI_PASSWORD = "PASSWORD"
 
-def wifi_reset():   # Reset wifi to AP_IF off, STA_IF on and disconnected
-  sta = network.WLAN(network.WLAN.IF_STA); sta.active(False)
-  ap = network.WLAN(network.WLAN.IF_AP); ap.active(False)
-  sta.active(True)
-  while not sta.active():
-      time.sleep(0.1)
-  sta.disconnect()   # For ESP8266
-  while sta.isconnected():
-      time.sleep(0.1)
-  return sta, ap
+def wifi_reset():  # Reset wifi to AP_IF off, STA_IF on and disconnected
+    sta = network.WLAN(network.WLAN.IF_STA)
+    sta.active(False)
+    ap = network.WLAN(network.WLAN.IF_AP)
+    ap.active(False)
+    sta.active(True)
+    while not sta.active():
+        time.sleep(0.1)
+    sta.disconnect()  # For ESP8266
+    while sta.isconnected():
+        time.sleep(0.1)
+    return sta, ap
+
 
 sta, ap = wifi_reset()
 
@@ -29,7 +32,7 @@ sta.config(pm=sta.PM_NONE)  # ..then disable power saving
 
 print("Proxy running on channel:", sta.config("channel"))
 
-ap.active(True) # ESP8266 peers must send messages to this AP_IF interface
+ap.active(True)  # ESP8266 peers must send messages to this AP_IF interface
 
 mac_address_str = binascii.hexlify(ap.config("mac"), ":").decode().upper()
 print("MAC address:", mac_address_str)
@@ -44,7 +47,7 @@ while True:
     host, msg = e.recv()
     if msg == b"ring":
         print("Doorbell signal received! Sending webhook...")
-        
+
         try:
             r = urequests.post(WEBHOOK_URL)
             print("Webhook sent! Response:", r.status_code)
